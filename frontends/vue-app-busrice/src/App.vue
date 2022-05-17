805a98f91be6f53a756796cd685d4e86d4567c60
Alisha <thanapol.kst@gmail.com>
2022-05-17 19:01:10 +0700

    first commit

---
+++ frontends/vue-app-busrice/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ store.doubleCount }}
+    <button @click="store.increment">add</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+import { watch } from "vue";
+const store = useCounterStore();
+
+store.$subscribe((mutation, state) => {
+ console.log( mutation.events);
+ 
+  localStorage.setItem("count", JSON.stringify(state));
+});
+</script>
+
+<style scoped></style>
